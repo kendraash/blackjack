6fba202cacc3f8e39693fdeceeadd189307c19c4
Kendra Ash & Alyssa Horocks <student@epicodus.com>
2015-12-17 11:57:09 -0800

    Very loose representation of black jack complete. Super wet and terrible. Only for testing practice purposes. No judging.

--- js/scripts.js
+++ js/scripts.js
@@ -23,20 +23,37 @@ Game.prototype.gameOver = function() {
 
 
 $(document).ready(function() {
-  $("#newGame").click(function() {
     var game = new Game(0, 0);
 
-    game.deal("player");
-    game.deal("player");
-    game.deal("dealer");
-    game.deal("dealer");
+    $("#newGame").click(function() {
+      game.playerScore = 0;
+      game.dealerScore = 0;
 
-    var playerScore = (game.playerScore);
-    var dealerScore = (game.dealerScore);
+      game.deal("player");
+      game.deal("player");
+      game.deal("dealer");
+      game.deal("dealer");
 
-    $("#playerScore").html("" + playerScore + "");
-    $("#dealerScore").html("" + dealerScore + "");
+      var playerScore = (game.playerScore);
+      var dealerScore = (game.dealerScore);
 
+      $("#playerScore").html("" + playerScore + "");
+      $("#dealerScore").html("" + dealerScore + "");
+    });
 
-  });
+    $("#deal").click(function() {
+      if (game.gameOver() === false) {
+        game.deal("player");
+        game.deal("dealer");
+
+        $("#playerScore").html("" + game.playerScore + "");
+        $("#dealerScore").html("" + game.dealerScore + "");
+
+        if (game.gameOver() === true ) {
+          alert('Game is over.');
+        }
+      } else {
+        alert('Game is over.');
+      }
+    });
 });
